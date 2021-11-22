42b8d691e019266ec70fbefb307abddb8ec4c837
MTGia <free.zamd.150@gmail.com>
2021-11-22 18:40:14 +0900

    #2-create_add_page_fixed3

--- src/CreateComponent/Create.js
+++ src/CreateComponent/Create.js
@@ -1,6 +1,5 @@
 import {useState} from 'react'
 import './Create.css'
-import Breadcrumb from "../components/Breadcrumb";
 
 function Create() {
   // タイトル・詳細・IDを格納する変数
@@ -12,48 +11,49 @@ function Create() {
   return (
     <div className="create-container">
       {/* {headrエリア} */}
-      <Breadcrumb breadcrumbElements={breadcrumbElements} />
 
       <div className="contents-container">
 
         <div className="todo-create">
 
-          <div className="task-container input-container">
-            <label className="task-label input-area-label">タスク名 :</label>
+          <div className="task-container input-container content-container">
+            <label className="task-label input-area-label label">タスク名 :</label>
             <input
                 className="title-input input"
                 type="text"
                 label="タイトル"
+                rows="1"
                 value={todoTitle}
-                onChange={"タイトル取得関数：handleAddFormChanges"}
+                // 入力値セットする関数を後で入れると入力可能
             />
           </div>
 
-          <div className="text-container input-container">
-            <label className="text-label input-area-label">内容 :</label>
+          <div className="text-container input-container content-container">
+            <label className="text-label input-area-label label">内容 :</label>
             <textarea
                 className="text-input input"
                 type="text"
                 label="内容"
+                rows="20"
                 value={todoDetail}
-                onChange={"詳細取得関数：handleAddFormChangesForDetail"}
+                // 入力値セットする関数を後で入れると入力可能
             ></textarea>
           </div>
 
-          <div className="priority-container select-container input-container">
-            <label className="priority-label input-area-label">優先度 :</label>
+          <div className="priority-container select-container input-container content-container">
+            <label className="priority-label input-area-label label">優先度 :</label>
             <select className="select-priority select-box">
-              <option className="select-default">--------------------</option>
-              <option className="high">高</option>
-              <option className="middle">中</option>
-              <option className="low">低</option>
+              <option className="select-default option">--------------------</option>
+              <option className="high option">高</option>
+              <option className="middle option">中</option>
+              <option className="low option">低</option>
             </select>
           </div>
 
-          <div className="btn-container">
-            <button className="back-button button" onClick={"入力値取得関数：backFunc"}>戻る</button>
+          <div className="btn-container content-container">
+            <button className="back-button button" onClick={"入力値取得関数"}>戻る</button>
             {/* テスト用テキスト 追加/保存(編集)ボタンを動的に切り替える */}
-            <button className="add-button button" onClick={"入力値取得関数：addTodo"}>追加</button>
+            <button className="add-button button" onClick={"入力値取得関数"}>追加</button>
           </div>
          </div>
       </div>
