fd61beae35d964c0b130e4c9c9b471dbb14e9e90
tetsu633 <tetsunishimura633@gmail.com>
2021-12-11 18:41:16 +0900

    package.json homepage URLを修正

--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -1,18 +1,17 @@
-export const ContentContainer = ({todo}) => {
-    const { title,detail,status,priority } = todo;
-    const detailArray = [
-        {name:"タスク名",description:title},
-        {name:"内容",description:detail},
-        {name:"ステータス",description:status},
-        {name:"優先度",description:priority}]
+export const ContentContainer = ({ todo }) => {
+  const { title, detail, status, priority } = todo;
+  const detailArray = [
+    { key: 1, name: "タスク名", description: title },
+    { key: 2, name: "内容", description: detail },
+    { key: 3, name: "ステータス", description: status },
+    { key: 4, name: "優先度", description: priority },
+  ];
+  return detailArray.map(({ key, name, description }) => {
     return (
-        detailArray.map(({name,description})=>{
-            return(
-                <div className="content-container">
-                    <p className="field-name">{name}</p>
-                    <p className="field-description">{description}</p>
-                </div>
-            )
-        })
-    )
-}
+      <div key={key} className="content-container">
+        <p className="field-name">{name}</p>
+        <p className="field-description">{description}</p>
+      </div>
+    );
+  });
+};
